26b86f91629847ac0fc5432592c47d8654906364
gam-ba <32080195+gam-ba@users.noreply.github.com>
2017-09-18 21:02:48 -0300

    Update and rename scrapper_a_pandas to scrapper_a_pandas.py

---
+++ scrapper_a_pandas.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+signatures_df = pd.DataFrame.from_dict(signatures, orient='columns', dtype=None) # Se recupera el diccionario que genera el scrapper.
+del signatures_df['num_comments'] # Se borra la columna porque sino va a ser el número total repetido.
+signatures_df.to_csv('nombre_de_archivo', sep='\t', encoding='utf-8') # Se guarda como CSV para seguir trabajando.
